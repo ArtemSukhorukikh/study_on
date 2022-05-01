dad257ebd645450777e2eac9e74188826ab8e6e7
ArtemSukhorukikh <des1337481@gmail.com>
2022-05-01 18:55:32 +0300

    Редактирование формы курса.

--- src/Form/CourseType.php
+++ src/Form/CourseType.php
@@ -16,26 +16,47 @@ class CourseType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('code',TextType::class, [
-                'label'=>'Код курса',
+            ->add(
+                'code',
+                TextType::class,
+                [
+                'label' => 'Код курса',
                 'constraints' => [
                     new NotBlank(message: 'Поле не может быть пустым.'),
                     new Length(max: 255, maxMessage: 'Код курса не должно превышать 255 символов.')
                 ],
-            ])
-            ->add('name', TextType::class, [
-                'label'=>'Название курса',
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                ]
+            )
+            ->add(
+                'name',
+                TextType::class,
+                [
+                'label' => 'Название курса',
                 'constraints' => [
                     new NotBlank(message: 'Поле не может быть пустым.'),
                     new Length(max: 255, maxMessage: 'Название курса не должно превышать 255 символов.')
                 ],
-            ])
-            ->add('description', TextareaType::class,[
+                'attr' => [
+                   'class' => 'form-control'
+                ],
+                ]
+            )
+            ->add(
+                'description',
+                TextareaType::class,
+                [
                 'label' => 'Описание курса.',
                 'constraints' => [
                     new NotBlank(message: 'Поле не может быть пустым.'),
+                ],
+                'attr' => [
+                   'class' => 'form-control'
+                ],
                 ]
-            ])
+            )
         ;
     }
 
