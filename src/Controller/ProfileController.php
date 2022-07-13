752a4aa297b556a2931f314f8dd9c46ad0d09d40
ArtemSukhorukikh <des1337481@gmail.com>
2022-07-13 18:10:14 +0300

    Лаб 7 без тестов

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CourseRepository;
 use App\Service\BillingClient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -26,6 +27,28 @@ class ProfileController extends AbstractController
             'controller_name' => 'ProfileController',
             'userName' => $userInfo->username,
             'userBalance' => $userInfo->balance,
+        ]);
+    }
+
+    #[Route('/profile/history', name: 'app_profile_history')]
+    public function indexHistory(BillingClient $client, CourseRepository $courseRepository): Response
+    {
+        $res = $this->client->getTransactions([], $this->getUser()->getApiToken());
+
+        $historyData = [];
+        foreach ($res as $data) {
+            $historyData[] = [
+                'type' => $data['type'],
+                'value' => $data['value'],
+                'created_at' => $data['created_at'],
+                'course' => $data['type'] !==
+                'free' && isset($data['code']) ? $courseRepository->findOneBy(['code' => $data['code']]) : null,
+                'expires_at' => $data['expires_at'] ?? null,
+            ];
+        }
+
+        return $this->render('profile/history.html.twig', [
+            'transactions' => $historyData
         ]);
     }
 }
