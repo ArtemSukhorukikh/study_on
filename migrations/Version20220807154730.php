946adfab74f32a220128243e22ea83f9b07c1c0e
ArtemSukhorukikh <des1337481@gmail.com>
2022-08-07 18:49:19 +0300

    миграции

--- migrations/Version20220807154730.php
+++ migrations/Version20220807154730.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220513095004 extends AbstractMigration
+final class Version20220807154730 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,6 +20,8 @@ final class Version20220513095004 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE course_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE lesson_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE course (id INT NOT NULL, code VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(1000) DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_169E6FB977153098 ON course (code)');
         $this->addSql('CREATE TABLE lesson (id INT NOT NULL, course_id INT NOT NULL, name VARCHAR(255) NOT NULL, content TEXT NOT NULL, number INT NOT NULL, PRIMARY KEY(id))');
@@ -32,6 +34,8 @@ final class Version20220513095004 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE lesson DROP CONSTRAINT FK_F87474F3591CC992');
+        $this->addSql('DROP SEQUENCE course_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE lesson_id_seq CASCADE');
         $this->addSql('DROP TABLE course');
         $this->addSql('DROP TABLE lesson');
     }
